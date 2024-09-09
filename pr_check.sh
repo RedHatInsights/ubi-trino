8781f4a564469821d9fdff651010b7c0dc55a2ff
Sam Doran <sdoran@redhat.com>
2024-09-09 13:30:25 -0400

    Simplify pr_check.sh
    Use app-interface for building images and do not change tag based on
    files changed.

--- pr_check.sh
+++ pr_check.sh
@@ -10,36 +10,18 @@ set -ex
 APP_NAME="hccm"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="trino"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE_REPO="quay.io"
-ORG="redhat-user-workloads"
-TENANT="cost-mgmt-dev-tenant"
+ORG="cloudservices"
 APP="ubi-trino"
-IMAGE="${IMAGE_REPO}/${ORG}/${TENANT}/${APP}"
+IMAGE="${IMAGE_REPO}/${ORG}/${APP}"
 EXTRA_DEPLOY_ARGS="--set-parameter trino/IMAGE=${IMAGE}"
 COMPONENTS="hive-metastore koku trino"  # specific components to deploy (optional, default: all)
 COMPONENTS_W_RESOURCES="hive-metastore koku trino"  # components which should preserve resource settings (optional, default: none)
-CHANGED_DIR="$WORKSPACE/files_changed"
-
-mkdir -p $CHANGED_DIR
-
-function check_for_file_changes() {
-    if [ -f $CHANGED_DIR/files_changed.txt ]; then
-        egrep "$1" $CHANGED_DIR/files_changed.txt &>/dev/null
-    else
-        null &>/dev/null
-    fi
-}
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
-git diff --name-only origin/main > $CHANGED_DIR/files_changed.txt
-if check_for_file_changes 'default|bin|Dockerfile|image_build_num.txt'
-then
-    source $CICD_ROOT/build.sh
-else
-    IMAGE_TAG=$(git log --no-merges --format=%H --max-count=1)
-fi
+source $CICD_ROOT/build.sh
 
 # source $CICD_ROOT/_common_deploy_logic.sh
 # export NAMESPACE=$(bonfire namespace reserve)
